37f05cbf41678bf2fc4e59f81f20754246e13cef
junkai121 <junkai121@outlook.com>
2022-05-25 01:21:30 +1000

    update comments

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 
 rm -rf ~/.cohod
 
-# Build genesis file incl account for passed address
+# Build genesis
 cohod init --chain-id=test test
 cohod keys add validator --keyring-backend="test"
 cohod add-genesis-account $(cohod keys show validator -a --keyring-backend="test") 100000000000000ucoho
 cohod gentx validator 100000000ucoho --keyring-backend="test" --chain-id=test
 cohod collect-gentxs
 
-# Start coho
+# Start node
 cohod start --pruning=nothing
