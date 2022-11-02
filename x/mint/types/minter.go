c176e9666785d85b91ea340d206206eefac810a9
junkai121 <junkai121@outlook.com>
2022-11-02 17:48:26 +0800

    add default minter params

--- x/mint/types/minter.go
+++ x/mint/types/minter.go
@@ -24,10 +24,10 @@ func InitialMinter(inflation sdk.Dec) Minter {
 }
 
 // DefaultInitialMinter returns a default initial Minter object for a new chain
-// which uses an inflation rate of 13%.
+// which uses an inflation rate of 40%.
 func DefaultInitialMinter() Minter {
 	return InitialMinter(
-		sdk.NewDecWithPrec(13, 2),
+		sdk.NewDecWithPrec(40, 2),
 	)
 }
 
