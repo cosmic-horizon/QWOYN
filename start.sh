6739857d35f6c325093293db33b0b85484a8052d
jqwoyn <jqwoyn@outlook.com>
2023-05-09 00:58:22 +0800

    update shell script after further testing

--- start.sh
+++ start.sh
@@ -3,13 +3,16 @@
 rm -rf ~/.qwoynd
 
 echo "weather leader certain hard busy blouse click patient balcony return elephant hire mule gather danger curious visual boy estate army marine cinnamon snake flight" > mnemonic.txt;
+echo "never chuckle bird almost jacket veteran weekend original rare habit point scorpion place gadget net train more plug upon pear renew mule material dynamic" > mnemonic2.txt;
 
 # Build genesis
 qwoynd init --chain-id=qwoyn-1 test
 qwoynd keys add validator --keyring-backend="test"
-qwoynd keys add user1 --recover --keyring-backend=test < mnemonic.txt;
+qwoynd keys add maintainer --recover --keyring-backend=test < mnemonic.txt;
+qwoynd keys add user1 --recover --keyring-backend=test < mnemonic2.txt;
 
 qwoynd add-genesis-account $(qwoynd keys show validator -a --keyring-backend="test") 1000000000000uqwoyn,1000000000000ucoho,1000000000000stake
+qwoynd add-genesis-account $(qwoynd keys show maintainer -a --keyring-backend="test") 1000000000000uqwoyn,1000000000000ucoho,1000000000000stake
 qwoynd add-genesis-account $(qwoynd keys show user1 -a --keyring-backend="test") 1000000000000uqwoyn,1000000000000ucoho,1000000000000stake
 qwoynd gentx validator 100000000stake --keyring-backend="test" --chain-id=qwoyn-1
 qwoynd collect-gentxs
