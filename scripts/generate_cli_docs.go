02ad2fa10877c62c86917503943c44c2f34477a4
jqwoyn <jqwoyn@outlook.com>
2023-06-09 01:48:15 +0800

    barberry update

--- scripts/generate_cli_docs.go
+++ scripts/generate_cli_docs.go
@@ -1,18 +1,18 @@
 package main
 
-import (
-	"log"
+// import (
+// 	"log"
 
-	"github.com/spf13/cobra/doc"
+// 	"github.com/spf13/cobra/doc"
 
-	"github.com/regen-network/regen-ledger/v5/app/client/cli"
-)
+// 	"github.com/regen-network/regen-ledger/v5/app/client/cli"
+// )
 
 // generate documentation for all regen app commands
 func main() {
-	rootCmd, _ := cli.NewRootCmd()
-	err := doc.GenMarkdownTree(rootCmd, "commands")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// rootCmd, _ := cli.NewRootCmd()
+	// err := doc.GenMarkdownTree(rootCmd, "commands")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
