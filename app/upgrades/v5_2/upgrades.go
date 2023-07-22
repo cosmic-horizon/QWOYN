461735a38c6a654d3ff02d73d0ff02cc0efafa2b
Daniel Pittman <dpdanpittman@gmail.com>
2023-07-22 11:16:58 -0600

    add 5.2 upgrade handler

---
+++ app/upgrades/v5_2/upgrades.go
@@ -0,0 +1,28 @@
+package v5_2 //nolint:revive,stylecheck
+
+import (
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/group"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	"github.com/cosmic-horizon/QWOYN/v5_2/app/upgrades"
+)
+
+const Name = "v5.2"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName: Name,
+	CreateUpgradeHandler: func(mm *module.Manager, cfg module.Configurator) upgradetypes.UpgradeHandler {
+		return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+
+			return mm.RunMigrations(ctx, cfg, fromVM)
+		}
+	},
+	StoreUpgrades: storetypes.StoreUpgrades{
+		Added: []string{
+			group.ModuleName,
+		},
+	},
+}
